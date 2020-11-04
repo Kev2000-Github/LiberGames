291f7499b21c70218a35c3c7ce18ad3b66403fca
Kevin <chengkev2000@gmail.com>
2020-11-04 12:10:37 -0400

    added error handling to endpoints

--- back-end/src/index.js
+++ back-end/src/index.js
@@ -6,10 +6,19 @@ const {mongoose}=require('./database');
 
 //SETTINGS
 app.set('port',process.env.PORT || 3000);
+/* app.set('whiteList', ['http://localhost:8080/']);
+app.set('corsOptions',{
+    origin: (origin,callback)=>{
+        if(app.get('whiteList').indexOf(origin)!=-1) callback(null,true);
+        else callback("Not allowed by CORS policy");
+    }
+})
+ */
 
-//MIDDLEWARES
+ //MIDDLEWARES
 app.use(express.json());
 app.use(morgan('dev'));
+//app.use(cors(app.get('corsOptions')));
 
 //ROUTES
 app.use('/json',require('./routes/gameroutes'));
@@ -17,7 +26,6 @@ app.use('/json',require('./routes/gameroutes'));
 //STATIC FILES
 app.use(express.static(path.join(__dirname,'public')));
 
-
-app.listen(app.get('port'),()=>{
+app.listen(app.get('port'), ()=>{
     console.log(`the server is up on port ${app.get('port')}`);
 })
--- back-end/src/routes/gameroutes.js
+++ back-end/src/routes/gameroutes.js
@@ -2,8 +2,10 @@ const express=require('express');
 const { title } = require('process');
 const router=express.Router();
 const Game=require('../models/games');
+const jwt=require("jsonwebtoken");
+require('dotenv').config();
 
-router.get('/', async (req, res)=>{
+router.get('/',async (req, res)=>{
     const games=await Game.find();
     res.json(games);
 })
@@ -11,24 +13,46 @@ router.get('/', async (req, res)=>{
 router.post('/', async (req,res)=>{
     const data=req.body;
     const newGame=new Game({...data});
-    await newGame.save();
-    res.json({status: 'the game has been added'})
+    await newGame.save().then(()=>{
+        res.json({status: 'the game has been added'})
+    })
+    .catch(err=>{
+        console.log(err)
+        res.json({status:"Error"})
+    })
 })
 
 router.get('/:id', async (req,res)=>{
-    const game=await Game.findById(req.params.id);
-    res.json({game});
+    const game=await Game.findById(req.params.id).then(()=>{
+        res.json({game});
+    })
+    .catch(err=>{
+        console.log(err);
+        res.json({status:"Error, Game not found"})
+    })
 })
 
 router.put('/:id', async (req,res)=>{
     const data=req.body;
-    await Game.findByIdAndUpdate(req.params.id,{...data},{useFindAndModify:false});
-    res.json({status: "the game has been updates successfully"})
+    await Game.findByIdAndUpdate(req.params.id,{...data},{useFindAndModify:false})
+    .then(()=>{
+        res.json({status: "the game has been updates successfully"})
+    })
+    .catch(err=>{
+        console.log(err);
+        res.json({status:"Error"})
+    })
 })
 
 router.delete('/:id', async (req,res)=>{
-    await Game.findByIdAndDelete(req.params.id);
-    res.json({status:"the game has been deleted successfully"});
+    await Game.findByIdAndDelete(req.params.id).then(()=>{
+        res.json({status:"the game has been deleted successfully"});
+    })
+    .catch(err=>{
+        console.log(err);
+        res.json({status: "No game found"});
+    })
+    
 })
 
 module.exports=router;
